dc85a180b924664278d5c803163e20f8b68c03ec
Serhii Cherniavskyi <cherniavskyi.dev@gmail.com>
2023-10-12 17:48:08 +0300

    feat: added wheel zoom listener;

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useAnnotationAddListener } from './hooks/useAnnotationAddListener';
 import { useAnnotationDeleteListener } from './hooks/useAnnotationDeleteListener';
 import { useAnnotationModifyListener } from './hooks/useAnnotationsModifyListener';
 import { useInitViewer } from './hooks/useInitViewer';
-import { useScrollOnMouseListener } from './hooks/useScrollOnMouseWheelListener';
+import { useScrollOnMouseWheelListener } from './hooks/useScrollOnMouseWheelListener';
 
 function App() {
   const viewer = useRef(null);
@@ -25,17 +25,13 @@ function App() {
     ).then((webviewerInstance) => {
       setInstanceContext(webviewerInstance);
       initViewer(webViewerInstance);
-      // const deltaZoomIn = documentViewer.getZoomLevel() + 0.25;
-      // const maxZoomLevel = webviewerInstance.UI.getMaxZoomLevel();
-
-      // documentViewer.zoomTo(deltaZoomIn < maxZoomLevel ? deltaZoomIn : maxZoomLevel);
     });
   }, [initViewer, setInstanceContext, webViewerInstance]);
 
   useAnnotationAddListener();
   useAnnotationModifyListener();
   useAnnotationDeleteListener();
-  useScrollOnMouseListener();
+  useScrollOnMouseWheelListener();
 
   return (
     <div style={{ width: '100vw', height: '100vh', background: 'gray' }}>
--- src/hooks/useScrollOnMouseWheelListener.js
+++ src/hooks/useScrollOnMouseWheelListener.js
@@ -2,33 +2,28 @@ import { useCallback, useContext, useEffect } from 'react';
 
 import { WebviewerContext } from '../context/webviewer-context';
 
-export const useScrollOnMouseListener = () => {
+export const useScrollOnMouseWheelListener = () => {
   const { instance } = useContext(WebviewerContext);
-  let throttleTime;
 
   const handler = useCallback(() => {
     if (instance) {
-      // const getMouseLocation = (e) => {
-      //   const scrollElement =
-      //     instance.Core.documentViewer.getScrollViewElement();
-      //   const scrollLeft = scrollElement.scrollLeft || 0;
-      //   const scrollTop = scrollElement.scrollTop || 0;
-
-      //   return {
-      //     x: e.pageX + scrollLeft,
-      //     y: e.pageY + scrollTop,
-      //   };
-      // };
-
-      console.log();
+      const { documentViewer, DisplayMode } = instance.Core;
+      // const docDimensions = documentViewer.getDocument().getPageInfo(1);
       const scrollElement = instance.Core.documentViewer.getScrollViewElement();
-      // console.log('lol', scrollElement)
       scrollElement.addEventListener('wheel', (e) => {
         e.stopPropagation();
         e.preventDefault();
-        console.log(e);
+        console.log(e.offsetX);
+        console.log(e.offsetY);
+
+        if (e.deltaY < 0) {
+          documentViewer.zoomToMouse(documentViewer.getZoomLevel() * 1.1, e.offsetX, e.offsetY);
+          // documentViewer.zoomTo(documentViewer.getZoomLevel() * 1.01);
+        } else {
+          documentViewer.zoomToMouse(documentViewer.getZoomLevel() * 0.9, e.offsetX, e.offsetY);
+          // documentViewer.zoomTo(documentViewer.getZoomLevel() * 0.99);
+        }
       });
-      // instance.Core.documentViewer.zoomToMouse(2, 0, 0)
     }
   }, [instance]);
 
